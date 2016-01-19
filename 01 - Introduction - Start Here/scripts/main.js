1073d25d8a493f431242c67f1aa086a08af7ec8b
rittycheriah <rittycheriah@gmail.com>
2016-01-19 10:49:03 -0600

    first react component-- with comment notes

--- 01 - Introduction - Start Here/scripts/main.js
+++ 01 - Introduction - Start Here/scripts/main.js
@@ -1,7 +1,8 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 // storepicker component: this will let us make <StorePicker/>
-
+// This renders as a <p> but the <StorePicker/> becomes a 
+// reusable web component 
 var StorePicker = React.createClass({
 	render: function() {
 		return (
