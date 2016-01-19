599ac0cf6b08f6f6076068d16ba354f9338cd3c3
rittycheriah <rittycheriah@gmail.com>
2016-01-19 11:01:15 -0600

    starting form for first react page

--- 04 - Writing HTML with JSX/scripts/main.js
+++ 04 - Writing HTML with JSX/scripts/main.js
@@ -7,10 +7,15 @@ var ReactDOM = require('react-dom');
 */
 
 var StorePicker = React.createClass({
-
+	// always return a single element with many nested inside per JSX
   render : function() {
     return (
-      <p>hello</p>
+      <form>
+      	<h2>Please enter a store</h2>
+      	// react doesn't like self closing, put / in the end
+      	<input type="text" ref="storeId"/>
+      	<input type="Submit"/>
+      </form>
     )
   }
 });
